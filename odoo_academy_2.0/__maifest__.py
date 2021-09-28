e984ba55d04ec4c2da4f5d84d13c6df46e7f903e
emab-odoo <emab@odoo.com>
2021-09-28 12:58:44 -0500

    [ADD] new basic module

---
+++ odoo_academy_2.0/__maifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy 2.0',
+    'summary': """Odoo app to manage training""",
+    'description': 
+    """
+    Hehehehe:
+        - helo
+    """,
+    'author': 'emab',
+    'website':'https://odoo.com',
+    'version': '2.0',
+    'depends': ['base'],
+    'data': [],
+    'demo': []
+    
+}
